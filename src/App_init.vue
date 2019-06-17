310e136110109b99a324d18169c8f91c2f158f1c
SRFeesha <srfeesha@gmail.com>
2019-06-17 14:15:12 +0200

    freaks board w/o component

--- src/App_init.vue
+++ src/App_init.vue
@@ -1,42 +1,28 @@
 <template>
   <div id="app">
-    <h1>Freaks Board</h1>
-    <FreaksBoard/>
+    <img src="./assets/logo.png">
+    <HelloWorld/>
   </div>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld";
-import FreaksBoard from "./components/FreaksBoard";
 
 export default {
   name: "App",
   components: {
-    FreaksBoard
+    HelloWorld
   }
 };
 </script>
 
 <style>
-html,
-body {
-  margin: 0;
-  background-image: url("./assets/bg.jpg");
-  overscroll-behavior-y: none;
-}
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  /* text-align: center; */
+  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
-h1 {
-  text-align: center;
-  font-weight: 900;
-  font-size: 6rem;
-  color: #ffffffcc;
-  margin: 2rem 0;
 }
 </style>
