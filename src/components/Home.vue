05ec1fdcbb34fe7156e1e72b95ab5a3119544d08
mariacelestebonometti <mariac.bonometti@gmail.com>
2019-06-27 19:20:07 +0200

    nav init

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,14 +1,76 @@
 <template>
-  <div>
-    <h1>Home</h1>
-    <p>Here is some information. This is the home page</p>
+  <div class="intro">
+    <div>
+      <div>
+        <h1>A creative meetup</h1>
+        <p>We are calling out for Visual, UI/UX, and Interaction Design enthusiasts in the Trento community</p>
+        <button>Join meetup</button>
+      </div>
+      <img src="../assets/illustration_01_welcome.png" alt="Welcome">
+    </div>
+    <div>
+      <img src="../assets/illustration_02_who we are.png" alt="Who we are">
+      <div>
+        <h1>We are the design freaks</h1>
+        <p>We provide an opportunity for design students in Trento to exchange ideas, learn new skills and have fun while meeting new exciting creative minds.</p>
+      </div>
+    </div>
+    <div>
+      <div>
+        <h1>Do more of what you love, with us</h1>
+        <p>Events include talks from guest speakers on topics around design, photography and professional arts, networking opportunities, dynamic workshop and a good old-fashioned chin-wag over a beer or two.</p>
+        <button>Join meetup</button>
+      </div>
+      <img src="../assets/illustration_03_what we do.png" alt="What we do">
+    </div>
+    <div class="sponsor">
+      <h2>Proudly sponsored by</h2>
+    </div>
+    <div>
+      <img src="../assets/illustration_05_feedback.png" alt="Share your ideas">
+      <div>
+        <h1>Got an idea? Share it!</h1>
+        <p>Our community listen to new and old members alike. If you have an idea, a suggestion or a feature request, please let us know! You can find us litterally everywhere.</p>
+      </div>
+    </div>
+    <div>
+      <h2>Frequently Asked Questions</h2>
+      <div>
+        <h4>What's the meetup about?</h4>
+        <p></p>
+      </div>
+      <div>
+        <h4>Is the event open to designers only?</h4>
+        <p></p>
+      </div>
+      <div>
+        <h4>Where do I book?</h4>
+        <p></p>
+      </div>
+      <div>
+        <h4>Is the meetup completely free?</h4>
+        <p></p>
+      </div>
+      <div>
+        <h4>Who partecipates at these meetups?</h4>
+        <p></p>
+      </div>
+      <div>
+        <h4>Can I speak at the next event?</h4>
+        <p></p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Home' //this is the name of the component
-}
+  name: "Home" //this is the name of the component
+};
 </script>
 <style>
+.sponsor {
+  height: 50vh;
+  background-color: blueviolet;
+}
 </style>
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,45 +1,60 @@
-
 <template>
   <div>
-    <h2>Nav Bar</h2>
     <nav>
-        <router-link v-for="routes in links" 
-             v-bind:key="routes.id" 
-             :to="`${routes.page}`">{{ routes.text}}
-        </router-link>    
-</nav>
+    <img src="../assets/logo_00_designFreaksClub.png" alt="Logo DFC">
+      <router-link
+        class="nav-link"
+        v-for="routes in links"
+        v-bind:key="routes.id"
+        :to="`${routes.page}`"
+      >{{ routes.text}}</router-link>
+    </nav>
   </div>
 </template>
 
 <script>
-// import ...
 
 export default {
-    name: "Nav",
-    data() { 
-        return { 
-            links: [{ 
-                id: 0,
-                text: 'Home', 
-                page: '/' 
-            },
-            { 
-                id: 1,
-                text: 'Meetup', 
-                page: '/meetup' 
-            },
-            { 
-                id: 2,
-                text: 'Schedule', 
-                page: '/schedule' 
-            },
-            { 
-                id: 3,
-                text: 'Freaks Board', 
-                page: '/freaksboard' 
-            }
-            ] 
+  name: "Nav",
+  data() {
+    return {
+      links: [
+        {
+          id: 0,
+          text: "Home",
+          page: "/"
+        },
+        {
+          id: 1,
+          text: "Meetup",
+          page: "/meetup"
+        },
+        {
+          id: 2,
+          text: "Schedule",
+          page: "/schedule"
+        },
+        {
+          id: 3,
+          text: "FreaksBoard",
+          page: "/freaksboard"
         }
-    }
+      ]
+    };
+  }
 };
 </script>
+
+<style>
+nav {
+    background-color: pink;
+    height: 20vh;
+}
+.nav-link{
+    color:black;
+    background-color: red;
+}
+a.router-link-exact-active{
+    background-color: green;
+}
+</style>
