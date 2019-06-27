d4db97264e07502e6ce6de7db28d77712b1f0903
mariacelestebonometti <mariac.bonometti@gmail.com>
2019-06-27 13:47:52 +0200

    added routes

---
+++ src/components/Meetup.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Meetup</h1>
+    <p>Here is some information. This is the meetup page</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Meetup' //this is the name of the component
+}
+</script>
+<style>
+</style>
---
+++ src/components/Schedule.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Schedule</h1>
+    <p>Here is some information. This is the schedule page</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Schedule' //this is the name of the component
+}
+</script>
+<style>
+</style>
