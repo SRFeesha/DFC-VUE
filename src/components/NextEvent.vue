a672e7ab81cb53b00705a991c6687921e5a789e6
mariacelestebonometti <mariac.bonometti@gmail.com>
2019-09-02 13:03:41 +0200

    Added New Event info

--- src/components/NextEvent.vue
+++ src/components/NextEvent.vue
@@ -1,12 +1,22 @@
 <template>
-  <div class="contentSpace">
-    <h3>New event coming soon</h3>
-    <p>{{ date }}</p>
-    <h2>{{ title }}</h2>
-    <p>{{ resume }}</p>
-    <img src="" alt="Next event image">
-    <button>Tickets</button>
+<div>
+  <h3>New event coming soon</h3>
+  <div class="nextEvent">
+    <div class="infoEvent">
+      <p>{{ date }}</p>
+      <h2>{{ title }}</h2>
+      <p>{{ resume }}</p>
+      <div class="cta">
+        <button>Get ticket</button>
+      </div>
+    </div>
+    <div class="photoEvent">
+      <img  class="photo" src="https://www.raconteur.net/wp-content/uploads/2016/12/Design-thinking-1280x720.jpg" alt="Next event image">
+    </div>
   </div>
+  <div class="moreSpace">
+  </div>
+</div>
 </template>
 
 <script>
@@ -15,7 +25,7 @@ export default {
   name: "NextEvent",
   data() {
       return {
-          date: 'June 19, 9PM',
+          date: 'November 19, 9PM',
           title: 'Leading design',
           resume: 'Leading Design is a conference for people leading design teams, overseeing design direction, or instilling a culture of design within their organisations.'
       };
@@ -26,5 +36,28 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss">
+@media screen and (min-width: 768px) {
+  .nextEvent {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-template-rows: 100%;
+    padding: 3rem 0rem 6rem 0rem;
+    .infoEvent {
+      grid-column: 2 / 3;
+      padding: 0rem 2rem 0rem 2rem;
+
+    }
+    .photoEvent {
+      grid-column: 1 / 2;
+      grid-row: 1;
+      .photo {
+        max-width: 100%;
+      }
+    }
+  }
+  h3{
+    text-align:center;
+  }
+}
 </style>
